99122a5f83403cbec30be58f8f7c8722c4058859
Anthony <anth06ny@gmail.com>
2024-01-15 14:47:12 +0100

    Initial commit

---
+++ app/src/main/java/com/amonteiro/a01_supvincia/TempJava.java
@@ -0,0 +1,10 @@
+package com.amonteiro.a01_supvincia;
+
+import com.amonteiro.a01_supvincia.exo.ExoKotlinKt;
+
+public class TempJava {
+
+    public static void main(String[] args) {
+       //ExoKotlinKt.boulangerie();
+    }
+}
---
+++ app/src/main/java/com/amonteiro/a01_supvincia/exo/exoKotlin.kt
@@ -0,0 +1,64 @@
+package com.amonteiro.a01_supvincia.exo
+
+fun main() {
+    val res = boulangerie(
+        scanNumber("Nb croissant : "),
+        scanNumber("Nb sandwitch : "),
+        scanNumber("Nb baguette : ")
+    )
+    println("res=$res")
+
+
+}
+
+fun scanText(question: String): String {
+    print(question)
+    return readlnOrNull() ?: "-"
+}
+
+fun scanNumber(question: String): Int {
+    return scanText(question).toIntOrNull() ?: 0
+}
+
+fun boulangerie(nbC: Int = 0, nbS: Int = 0, nbBag: Int = 0) =
+    nbC * PRICE_CROI + nbS * PRICE_SAND + nbBag * PRICE_BAG
+
+fun myPrint(text: String) = println("#$text#")
+
+fun pair(c: Int) = c % 2 == 0
+
+fun min(a: Int, b: Int, c: Int) = if (a < b && a < c) a else if (b < a && b < c) b else c
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
