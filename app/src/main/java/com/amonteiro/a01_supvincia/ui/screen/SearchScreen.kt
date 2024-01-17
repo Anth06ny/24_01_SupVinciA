210db8d64f4a2fe0d45c806e5481dc5017553345
Anthony <anth06ny@gmail.com>
2024-01-17 16:54:52 +0100

    Correction TP Etat compose 1er exo

--- app/src/main/java/com/amonteiro/a01_supvincia/ui/screen/SearchScreen.kt
+++ app/src/main/java/com/amonteiro/a01_supvincia/ui/screen/SearchScreen.kt
@@ -33,7 +33,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -79,7 +78,6 @@ fun SearchScreenPreviewDark() {
 fun SearchScreen() {
     Column(
         modifier = Modifier
-            .background(Color.LightGray)
             .padding(8.dp)
     ) {
 
