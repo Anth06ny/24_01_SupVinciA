f8540e880eb5184a2c0dbc6514bcc3dd272501a6
Anthony <anth06ny@gmail.com>
2024-01-17 11:55:02 +0100

    Correction TP Compose Partie 1

--- app/src/main/java/com/amonteiro/a01_supvincia/ui/screen/SearchScreen.kt
+++ app/src/main/java/com/amonteiro/a01_supvincia/ui/screen/SearchScreen.kt
@@ -1,16 +1,31 @@
 package com.amonteiro.a01_supvincia.ui.screen
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.amonteiro.a01_supvincia.R
 import com.amonteiro.a01_supvincia.model.PictureData
 import com.amonteiro.a01_supvincia.model.pictureList
 import com.amonteiro.a01_supvincia.ui.theme._01_SupVinciATheme
+import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
+import com.bumptech.glide.integration.compose.GlideImage
+import com.bumptech.glide.integration.compose.placeholder
 
 //Code affiché dans la Preview
 @Preview(
@@ -35,10 +50,45 @@ fun SearchScreen() {
 }
 
 //Composable affichant 1 PictureData
+@OptIn(ExperimentalGlideComposeApi::class)
 @Composable
 fun PictureRowItem(modifier: Modifier = Modifier, data: PictureData) {
-    //TODO
-    Text(
-        text = "Hello World",
-    )
+    Row(modifier = Modifier
+        .padding(8.dp)
+        .fillMaxWidth()
+        .background(Color.White)
+        .heightIn(max = 100.dp)) {
+
+        GlideImage(
+            model = data.url,
+            //Dans string.xml
+            //contentDescription = getString(R.string.picture_of_cat),
+            //En dur
+            contentDescription = "une photo de chat",
+            loading = placeholder(R.mipmap.ic_launcher_round), // Image de chargement
+            // Image d'échec. Permet également de voir l'emplacement de l'image dans la Preview
+            failure = placeholder(R.mipmap.ic_launcher),
+            contentScale = ContentScale.Fit,
+            //même autres champs qu'une Image classique
+            modifier = Modifier
+                .heightIn(max = 100.dp) //Sans hauteur il prendra tous l'écran
+                .widthIn(max = 100.dp)
+        )
+
+        Spacer(modifier = Modifier.size(8.dp))
+
+        Column {
+            Text(
+                text = data.text,
+                fontSize = 20.sp
+            )
+            Text(
+                text = data.longText.take(20) + "...",
+                fontSize = 14.sp,
+                color = MaterialTheme.colorScheme.primary
+            )
+        }
+    }
+
+
 }
